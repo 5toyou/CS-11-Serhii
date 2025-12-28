4afd1eb8edcaca08a49b78b57f85c4f89a33d37a
5ToYou <sobkos934@gmail.com>
2025-12-28 21:34:35 +0200

    The super fast color palettes generator!

--- all_programs/page.js
+++ all_programs/page.js
@@ -9,20 +9,24 @@ document.getElementById("goto_index").onclick = () => {change_HTML("index.html")
 const colorful_case = document.querySelector(".colorful_case")
 
 const min_number = 0;
-const max_number = 360;
+const max_number = 255;
 
 
 function my_func(element){
-    random_color = Math.floor(Math.random() * (max_number - min_number + 1));
-    const rgb_color = `hsl(${random_color},100%,50%)`;
+    random_color_1 = Math.floor(Math.random() * (max_number - min_number + 1));
+    random_color_2 = Math.floor(Math.random() * (max_number - min_number + 1));
+    random_color_3 = Math.floor(Math.random() * (max_number - min_number + 1));
+    const rgb_color = `rgb(${random_color_1},${random_color_2},${random_color_3})`;
+
+    element.style.color = rgb_color;
+    element.classList.add("bounce");
 
     element.addEventListener("animationend",() => {
-        element.classList.remove("anim")
+        element.classList.remove("bounce");
     }) 
-    element.style = `--dynamic-1comma:${rgb_color}`;
-    element.classList.add("anim")
-}
-
-function blacker(element){
-    element.style.color = "black"
+    
+    setTimeout(() => {
+        element.style.color = "black";
+    }, 1500);
+    
 }
