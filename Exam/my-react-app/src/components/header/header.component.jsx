b0b3a04f472b845e799c55d16231914d0fec3257
5ToYou <sobkos934@gmail.com>
2026-01-02 22:02:58 +0200

    header and folders for homepage

--- Exam/my-react-app/src/components/header/header.component.jsx
+++ Exam/my-react-app/src/components/header/header.component.jsx
@@ -4,10 +4,12 @@ function Header() {
 
     return (
         <header className={classes.container}>
-            <div className={classes.title}>colors</div>
+            <div className={classes.title}>coolors</div>
             <div className={classes.menu}>
                 <div>tools</div>
                 <div>go pro</div>
+                <a>Sign in</a>
+                <a>Sign up</a>
             </div>
         </header>
     )
