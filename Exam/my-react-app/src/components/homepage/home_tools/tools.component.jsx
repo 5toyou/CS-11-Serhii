9af375d6f1bd1373486aaf512bbb59a697f2ca6c
5ToYou <sobkos934@gmail.com>
2026-01-03 17:10:09 +0200

    Tools are done for now

--- Exam/my-react-app/src/components/homepage/home_tools/tools.component.jsx
+++ Exam/my-react-app/src/components/homepage/home_tools/tools.component.jsx
@@ -3,10 +3,33 @@ import Tool from "./tools_tool/tool.component"
 
 function Tools() {
 
+    const cardData = 
+    [
+        { title: "Palette Generator", text: "Create beautiful color schemes...", link: "START THE GENERATOR", color: "#D1F9FF", textColor: "#007B8C" },
+        { title: "Explore Palettes", text: "Get inspired by thousands...", link: "EXPLORE 10M+ PALETTES", color: "#DDEBFF", textColor: "#2D5BFF" },
+        { title: "Image Picker", text: "Extract beautiful colors...", link: "LAUNCH THE IMAGE PICKER", color: "#F0E9FF", textColor: "#8A3FFC" },
+        { title: "Contrast Checker", text: "Calculate the contrast ratio...", link: "TRY THE CONTRAST CHECKER", color: "#FCE7F3", textColor: "#DB2777" },
+        { title: "Palette Visualizer", text: "Preview your colors on real designs...", link: "OPEN THE VISUALIZER", color: "#FFE4E1", textColor: "#E11D48" },
+        { title: "Color Picker", text: "Get useful color information...", link: "LAUNCH THE COLOR PICKER", color: "#FFEDD5", textColor: "#D97706" },
+        { title: "Tailwind Colors", text: "Preview Tailwind CSS colors on real designs to see how they look in context before using them in your projects.", link: "Get your Tailwind colors", color: "#fffee1ff", textColor: "#e1bd1dff" },
+        { title: "Color Bot", text: "Chat with our AI-powered Color Bot, ask questions and get color suggestions for your projects.", link: "Chat with Color Bot", color: "#e1ffe2ff", textColor: "#20e11dff" }
+    ];
+
     return (
-        <div>
+        <div className = {classes.style}>
             <div>OUR TOOLS, LOVED BY MILLIONS</div>
-            <Tool></Tool>
+            <div className = {classes.grider}>
+                {cardData.map((data, index) => (
+                    <Tool 
+                        key={index}
+                        title={data.title}
+                        text={data.text}
+                        link={data.link}
+                        color={data.color}
+                        textColor={data.textColor}
+                    />
+                ))}
+            </div>
         </div>
     )
 
--- Exam/my-react-app/src/components/homepage/home_tools/tools_tool/tool.component.jsx
+++ Exam/my-react-app/src/components/homepage/home_tools/tools_tool/tool.component.jsx
@@ -1,10 +1,20 @@
 import classes from "./tool.module.css"
 
-function Tool() {
+function Tool({ title, text, link, color, textColor}) {
+
+    const dynamicStyle = {
+        "--bg-initial": color,
+        "--text-initial": textColor
+    };
 
     return (
-        <div>
-            <div>tool</div>
+        <div className = {classes.card} style={ dynamicStyle }>
+            <div>{title}</div>
+            <div>{text}</div>
+            <div className = {classes.bottom}>
+                <div>{link}</div>
+                <div>→</div>
+            </div>
         </div>
     )
 
