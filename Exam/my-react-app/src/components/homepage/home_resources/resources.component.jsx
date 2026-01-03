8908ff2abd8a83b04d9ddc652eb2006aee175ee1
5ToYou <sobkos934@gmail.com>
2026-01-03 17:36:59 +0200

    resources start files

---
+++ Exam/my-react-app/src/components/homepage/home_resources/resources.component.jsx
@@ -0,0 +1,17 @@
+import classes from "./resources.module.css"
+
+
+function Resources() {
+
+    return (
+        <div className = {classes.style}>
+            <div>More useful resources</div>
+            <div>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Resources
---
+++ Exam/my-react-app/src/components/homepage/home_resources/resources_resource/resource.component.jsx
@@ -0,0 +1,18 @@
+import classes from "./resource.module.css"
+
+
+function Resource() {
+
+    return (
+        <div className = {classes.style}>
+            <div>More useful resources</div>
+            <div>
+
+            </div>
+        </div>
+    )
+
+}
+
+
+export default Resource
