255bb52d79b2b8efaec28293bdcab693906c013e
5ToYou <sobkos934@gmail.com>
2026-01-04 19:41:20 +0200

    registerpage DONE

--- Exam/my-react-app/src/components/registerpage/register.component.jsx
+++ Exam/my-react-app/src/components/registerpage/register.component.jsx
@@ -1,10 +1,17 @@
 import classes from "./register.module.css"
 import mustHave from "../must_have.module.css"
 import { useNavigate } from "react-router-dom"
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
+import { useState } from 'react';
 
 import bg_img from "../../assets/bg.png"
 
 function Register() {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
     const navigate = useNavigate();
 
@@ -15,9 +22,14 @@ function Register() {
                 <div className = {classes.register_box}>
                     <div>Sign up</div>
                     <div>Create a free account with your email.</div>
+
                     <input name="full_name" type="text" placeholder="Full Name" />
                     <input name="email" type="text" placeholder="Email" />
-                    <input name="password" type="text" placeholder="Password" />
+                    <div className={classes.pass_pos}>
+                        <input id="password" name="password" type={showPassword ? "text" : "password"} placeholder="Password"  />
+                        <button id="blur_btn" className = {classes.eyeButton} onClick={togglePasswordVisibility} >{showPassword ? <FaEyeSlash /> : <FaEye />}</button>
+                    </div>
+
                     <a onClick={() => navigate('/')} className = {mustHave.btn_blue}>Create your free account</a>
                     <div>Already have an account? Sign in</div>
                 </div>
