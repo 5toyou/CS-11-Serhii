e5706791eed611b80a99973b2781e1a343378baa
5ToYou <sobkos934@gmail.com>
2026-01-09 18:20:04 +0200

    header

--- Lab7/lab7-project/src/components/header.component/header.component.jsx
+++ Lab7/lab7-project/src/components/header.component/header.component.jsx
@@ -4,12 +4,7 @@ function HeaderComponent() {
 
     return (
         <header>
-            <div className = {classes.topRow}>
-                <a>Log in</a>
-                <a>Create an account</a>
-                <a>Check out</a>
-            </div>
-            <div className = {classes.bottomRow}>
+            <div className = {classes.leftSide}>
                 <div className = {classes.mainText}>
                     <div className = {classes.title}>
                         <div>Furniture</div>
@@ -17,19 +12,27 @@ function HeaderComponent() {
                     </div>
                     <div>The biggest choice on the web</div>
                 </div>
-                <div className = {classes.barGap}>
-                    <div className = {classes.cartBar}>
-                        <div>
-                            <img src="" alt="" />
-                        </div>
-                        <div>
-                            <div>My cart:</div>&nbsp;
-                            <div>0 item(s) -</div>&nbsp;
-                            <div>$0.00</div>
-                        </div>
+            </div>
+
+            <div className = {classes.rightSide}>
+                <div>
+                    <a>Log in</a>
+                    <a>Create an account</a>
+                    <a>Check out</a>
+                </div> 
+                <div className = {classes.cartBar}>
+                    <div>
+                        <img src="" alt="" />
                     </div>
-                    <div className = {classes.searchBar}>
-                        <input type="text" placeholder='Search store...' />
+                    <div>
+                        <div>My cart:</div>&nbsp;
+                        <div>0 item(s) -</div>&nbsp;
+                        <div>$0.00</div>
+                    </div>
+                </div>
+                <div className = {classes.searchBar}>
+                    <input type="text" placeholder='Search store...' />
+                    <div>
                         <img src="" alt="" />
                     </div>
                 </div>
